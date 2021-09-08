22a6c3ad6d2e9a4a2a2e99fd1cd4134d50872543
DESKTOP-G0L5N2I\user <seongguk.yoon@ontacthealth.com>
2021-09-08 19:15:15 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Renderfood(dish){
+  return( 
+  <Food name = {dish.name} picture = {dish.image} />
+  )
+}
+
+
+function Food({name, picture}){
+  return(
+  <div>
+    <h2>l like {name}</h2>
+    <img src = {picture}/>
+ </div>
+  )
+}
+
+const foodLike =[
+  {
+    id: 1,
+    name: "Kimchi",
+    image: "https://cdn.crowdpic.net/detail-thumb/thumb_d_CDC14868821FF3F20C77BC8BC15E6355.jpg"
+  },
+  {
+    id: 2,
+    name: "Kimbab",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS0_5woTu2Kk3_SjZWLva_W_QsxKTL1zNo5TXgpWbtf8h_y2rjH"
+  },
+  {
+    id: 3,
+    name: "Samgyeobsal",
+    image: "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg"
+  }
+]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+
+     {foodLike.map(Renderfood)}
+    
     </div>
   );
+
 }
+
 
 export default App;
